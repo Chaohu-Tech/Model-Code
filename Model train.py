a8655377bc7b9f483c59069577c57722909e3ad1
Chaohu-Tech <673745524@qq.com>
2024-07-15 18:55:50 +0800

    Update Model train.py

--- Model train.py
+++ Model train.py
@@ -1,6 +1,7 @@
+
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"      #双显卡
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"      # Dual GPU
 import tensorflow as tf
 from keras import layers, models
 from tensorflow.python.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
@@ -11,8 +12,7 @@ import cv2
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-
-#设置参数
+# Set parameters
 path = 'E:/model/dead_model/'
 
 dicClass = {'notdead': 0,
@@ -21,19 +21,18 @@ dicClass = {'notdead': 0,
 classnum = 2
 EPOCHS = 30
 
-
-#下面都不用改
+# The following do not need to be changed
 labelList = []
 INIT_LR = 0.0001
 Batch_Size = 128
 np.random.seed(123)
 datapath = path + 'train'
 
-#加载图片
-#和以前做法不同的是，这里不再处理图片，而是只返回图片路径的list列表
+# Load images
+# Unlike previous methods, here we do not process the images, but only return a list of image paths
 def loadImageData():
     imageList = []
-    listClasses = os.listdir(datapath)    # 类别文件夹
+    listClasses = os.listdir(datapath)    # Class folders
     print(listClasses)
     for class_name in listClasses:
         label_id = dicClass[class_name]
@@ -45,18 +44,16 @@ def loadImageData():
             imageList.append(image_full_path)
     return imageList
 
-
-print("开始加载数据")
+print("Starting to load data")
 imageArr = loadImageData()
 labelList = np.array(labelList)
-print("加载数据完成")
+print("Data loading complete")
 
-#random split
+# Random split
 trainX, valX, trainY, valY = train_test_split(imageArr, labelList, test_size=0.2, random_state=123)
 
-
-#定义图像处理的方法
-def generator(file_pathList, labels, batch_size, train_action = False):
+# Define image processing method
+def generator(file_pathList, labels, batch_size, train_action=False):
     L = len(file_pathList)
     while True:
         input_labels = []
@@ -80,9 +77,7 @@ def generator(file_pathList, labels, batch_size, train_action = False):
         batch_y = np.asarray(input_labels)
         yield (batch_x, batch_y)
 
-
-
-# 构造网络模型
+# Construct the network model
 model = models.Sequential([
     layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(512, 512, 3)),
     layers.MaxPooling2D(2,2),
@@ -112,16 +107,13 @@ model = models.Sequential([
     layers.Dense(classnum, activation='softmax'),
 ])
  
-
-# 查看网络结构
+# View the network structure
 model.summary()
 
-
-#定义损失函数优化器
-model.compile(optimizer = Adam(learning_rate = INIT_LR),
+# Define loss function and optimizer
+model.compile(optimizer=Adam(learning_rate=INIT_LR),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
 
 checkpointer = ModelCheckpoint(filepath=path + 'best_model.hdf5',
                                monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
@@ -131,24 +123,22 @@ reduce = ReduceLROnPlateau(monitor='val_accuracy', patience=10,
                            factor=0.5,
                            min_lr=1e-6)
 
-
-#模型训练
+# Model training
 history = model.fit(generator(trainX, trainY, Batch_Size, train_action=True),
-                    steps_per_epoch = len(trainX) / Batch_Size,
-                    validation_data = generator(valX, valY, Batch_Size, train_action=False),
-                    epochs = EPOCHS,
-                    validation_steps = len(valX) / Batch_Size,
-                    callbacks = [checkpointer, reduce])
+                    steps_per_epoch=len(trainX) / Batch_Size,
+                    validation_data=generator(valX, valY, Batch_Size, train_action=False),
+                    epochs=EPOCHS,
+                    validation_steps=len(valX) / Batch_Size,
+                    callbacks=[checkpointer, reduce])
 
 model.save(path + 'my_model.h5')
 print(history)
 
-
-#保留训练结果，并将其生成图片
+# Save training results and generate plots
 loss_trend_graph_path = path + 'WW_loss.png'
 acc_trend_graph_path = path + 'WW_acc.png'
-print("开始绘图")
-# summarize history for accuracy
+print("Starting to plot")
+# Summarize history for accuracy
 fig = plt.figure(1)
 plt.plot(history.history["accuracy"])
 plt.plot(history.history["val_accuracy"])
@@ -158,7 +148,7 @@ plt.xlabel("epoch")
 plt.legend(["train", "val"], loc="upper left")
 plt.savefig(acc_trend_graph_path)
 plt.close(1)
-# summarize history for loss
+# Summarize history for loss
 fig = plt.figure(2)
 plt.plot(history.history["loss"])
 plt.plot(history.history["val_loss"])
@@ -168,103 +158,4 @@ plt.xlabel("epoch")
 plt.legend(["train", "val"], loc="upper left")
 plt.savefig(loss_trend_graph_path)
 plt.close(2)
-print("绘图完成")
-
-
-
-# ##批量预测
-# import os
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"      #双显卡
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.preprocessing.image import img_to_array
-# import cv2
-# import numpy as np
-# from tqdm import tqdm
-# import csv
-# from keras import Model
-# import pandas as pd
-
-# path = 'G:/Downloads/pannuke_cut/'
-# save_path = os.path.join(path, 'test')
-# predict_dir = os.path.join(save_path, 'Dead')
-
-# model = load_model(path + 'my_model.h5')
-# #用model.summary()查看最后一层卷积层的名称
-# last_conv_layer_name = 'conv2d_46'
-
-# model_labels = {0: 'Neoplastic',
-#                 1: 'Inflammatory',
-#                 2: 'Connective',
-#                 3: 'Dead',
-#                 4: 'Epithelial'}
-
-
-# imagelist = pd.DataFrame()
-# denselist = pd.DataFrame()
-
-# index = -3
-# layer_model = Model(inputs=model.input, outputs=model.layers[index].output)
-
-
-# test = os.listdir(predict_dir)
-# for file in tqdm(test):
-#     filepath=os.path.join(predict_dir,file)
-#     image = cv2.imdecode(np.fromfile(filepath, dtype=np.uint8), -1)
-#     image = img_to_array(image)
-#     image = image[np.newaxis,:,:,:]
-    
-#     out = model.predict(image)  
-#     out = pd.DataFrame(out)
-#     pre = np.argmax(out)
-#     class_name_list = model_labels[pre]
-    
-#     out.insert(0, 'file', file) 
-#     out.insert(out.shape[1], 'pre_label', pre) 
-#     out.insert(out.shape[1], 'predict', class_name_list) 
-    
-#     imagelist = imagelist.append(out, ignore_index = True)
-    
-#     Dense = layer_model.predict(image)
-#     Dense = pd.DataFrame(Dense)
-#     Dense.insert(0, column = 'sample', value = file)
-#     denselist = denselist.append(Dense, ignore_index = True)
-    
-
-# imagelist.to_csv(save_path + 'Dead_predict.csv')
-# denselist.to_csv(save_path + 'Dead_tezheng.csv')
-
-
- 
-# #可以指定某一层的输出
-# from keras import Model
-# index = -3
-# layer_model = Model(inputs=model.input, outputs=model.layers[index].output)
-# predict = layer_model.predict(image)
-
-# model.layers
-# Out[65]: 
-# [<keras.layers.convolutional.Conv2D at 0x1fa8b7ec4e0>,
-#  <keras.layers.pooling.MaxPooling2D at 0x1fd63947240>,
-#  <keras.layers.convolutional.Conv2D at 0x1fd6394c5c0>,
-#  <keras.layers.pooling.MaxPooling2D at 0x1fd640b5630>,
-#  <keras.layers.convolutional.Conv2D at 0x1fd640b5d68>,
-#  <keras.layers.pooling.MaxPooling2D at 0x1fad61acfd0>,
-#  <keras.layers.convolutional.Conv2D at 0x1fab9d93a90>,
-#  <keras.layers.pooling.MaxPooling2D at 0x1fd63944278>,
-#  <keras.layers.core.Flatten at 0x1fd6392b978>,
-#  <keras.layers.core.Dense at 0x1fd63916898>,
-#  <keras.layers.core.Dropout at 0x1fd640a8ac8>,
-#  <keras.layers.core.Dense at 0x1fd640a1710>]
-
-# model.layers[-1]
-# Out[66]: <keras.layers.core.Dense at 0x1fd640a1710>
-
-# model.layers[-2]
-# Out[67]: <keras.layers.core.Dropout at 0x1fd640a8ac8>
-
-# model.layers[-3]
-# Out[68]: <keras.layers.core.Dense at 0x1fd63916898>
-
-# model.layers[-4]
-# Out[69]: <keras.layers.core.Flatten at 0x1fd6392b978>
+print("Plotting complete")
